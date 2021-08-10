98fe8f2ea66cdc016810d869236b8572c6f6678e
Kerolos Bebawy <omar.el-halawany@hotmail.com>
2021-08-10 15:54:15 +0200

    added restart app to valid channels

--- public/preload.js
+++ public/preload.js
@@ -7,13 +7,13 @@ contextBridge.exposeInMainWorld(
     "electron", {
     request: (channel, data) => {
         // whitelist channels
-        let validChannels = ["app_version", "update_available", "update_downloaded"];
+        let validChannels = ["app_version", "update_available", "update_downloaded", "restart_app"];
         if (validChannels.includes(channel)) {
             ipcRenderer.send(channel, data);
         }
     },
     response: (channel, func) => {
-        let validChannels = ["app_version", "update_available", "update_downloaded"];
+        let validChannels = ["app_version", "update_available", "update_downloaded", "restart_app"];
         if (validChannels.includes(channel)) {
             // Deliberately strip event as it includes `sender` 
             ipcRenderer.on(channel, (event, ...args) => func(...args));
